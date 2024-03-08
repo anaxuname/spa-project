220e24de83b404cdd0946e07b5a194addc9a9d52
Anaxuname <anaxuname@gmail.com>
2024-03-08 23:16:14 +0300

    added users app

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    username = None
+    first_name = None
+    last_name = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    full_name = models.CharField(max_length=255, verbose_name="Full Name")
+    phone_number = models.CharField(max_length=50, verbose_name="Phone_number")
+    city = models.CharField(max_length=50, default="Moscow", verbose_name="City")
+    avatar = models.ImageField(upload_to="users", blank=True, null=True, verbose_name="Avatar")
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
