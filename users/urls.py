464bc8ef5a02e9ab084e9c0ca2293214b5cda0f5
Anaxuname <anaxuname@gmail.com>
2024-03-10 22:38:08 +0300

    added users viewset

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,14 @@
 from users.apps import UsersConfig
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+from users.views import UserViewSet
 
 app_name = UsersConfig.name
-urlpatterns = []
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet, basename="user")
+
+urlpatterns = [
+    path(" ", include(router.urls)),
+] + router.urls
--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from users.serializers import UserSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserViewSet(viewsets.ViewSet):
+    """
+    Простой ViewSet-класс для вывода списка пользователей и информации по одному объекту
+    """
+
+    def list(self, request):
+        # Метод для вывода списка пользователей с определением выборки из базы и указанием сериализатора
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        # Метод для вывода информации по пользователю с определением выборки из базы и указанием сериализатора
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
