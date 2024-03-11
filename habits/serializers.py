91ae1745463d9d774e668d21fd603c9e746baa88
Anaxuname <anaxuname@gmail.com>
2024-03-11 22:42:01 +0300

    added model habit

---
+++ habits/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from habits.models import Habit
+
+
+class HabitSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор привычки
+    """
+
+    class Meta:
+        model = Habit
+        fields = "__all__"
