3e0e97b8b62b63084e93e0d89f6384ef007a1bcb
Anaxuname <anaxuname@gmail.com>
2024-03-17 00:47:30 +0300

    added habits serialisers

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 
 from habits.models import Habit
+from habits.validators import continiance_validator, periodicity_validator
 
 
 class HabitSerializer(serializers.ModelSerializer):
     """
     Сериализатор привычки
     """
+
+    continuance = serializers.IntegerField(validators=[continiance_validator])
+    periodicity = serializers.IntegerField(validators=[periodicity_validator])
 
     class Meta:
         model = Habit
