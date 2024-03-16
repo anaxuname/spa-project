56b8b052ebb90cb8e46669dec14aca188a8f01f6
Anaxuname <anaxuname@gmail.com>
2024-03-17 00:50:35 +0300

    added drf_yasg and simplejwt

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,19 @@
 from users.apps import UsersConfig
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from users.views import UserViewSet
 
 app_name = UsersConfig.name
 
 router = DefaultRouter()
-router.register(r"users", UserViewSet, basename="user")
+router.register("", UserViewSet, basename="user")
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ] + router.urls
