8c0a245427f440322cf47c881bb86428cbb74a1d
Anaxuname <anaxuname@gmail.com>
2024-03-21 01:24:31 +0300

    fixed model user

--- users/models.py
+++ users/models.py
@@ -13,8 +13,10 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name="Email")
     full_name = models.CharField(max_length=255, verbose_name="Full Name")
     phone_number = models.CharField(max_length=50, verbose_name="Phone_number")
-    chat_id = models.CharField(max_length=50, blank=True, null=True, verbose_name="Chat_id")
-    avatar = models.ImageField(upload_to="users", blank=True, null=True, verbose_name="Avatar")
+    chat_id = models.CharField(max_length=50, verbose_name="Chat_id")
+    avatar = models.ImageField(
+        upload_to="users", blank=True, null=True, verbose_name="Avatar"
+    )
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
